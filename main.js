16ddfe3f2668be952b9d778303824ed9573e78d2
vuxuanlam <lam.vuxuan2512@gmail.com>
2018-11-07 19:11:50 +0900

    add IPC, Application Menu, Context Menu and accelerator- global shortcuts

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const electron = require('electron')
 const app = electron.app
+const ipc = electron.ipcMain
 const BrowserWindow = electron.BrowserWindow
+const dialog = electron.dialog
+const Menu = electron.Menu
+const MenuItem = electron.MenuItem
+const globalShortcut = electron.globalShortcut
+
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
 let mainWindow1, mainWindow2, parentWindow, childWindow;
 
@@ -17,6 +23,15 @@ function createWindow() {
   mainWindow2.on('closed', () => {
     mainWindow2 = null
   })
+
+  ipc.on('open-error-dialog', function (event) {
+    dialog.showErrorBox('An error message', 'Demo of an error message')
+    event.sender.send('opened-error-dialog', 'Main process opened the error dialog')
+  })
+
+  ipc.on('sync-message', function (event) {
+    event.returnValue = 'sync-reply';
+  })
   mainWindow2.once('ready-to-show', () => {
     mainWindow2.show();
   })
@@ -29,7 +44,71 @@ function createWindow() {
   // });
 }
 
-app.on('ready', createWindow)
+app.on('ready', function () {
+  createWindow();
+  const template = [
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'pasteandmatchstyle' },
+        { role: 'delete' },
+        { role: 'selectall' }
+      ]
+    },
+    {
+      label: 'demo',
+      submenu: [
+        {
+          label: 'sub menu 1',
+          click: function () {
+            console.log('CLick Sub Menu 1');
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'sub menu 2'
+        }
+      ]
+    },
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'about electron',
+          click: function () {
+            electron.shell.openExternal('http://electron.atom.io')
+          },
+          accelerator: 'CmdOrCtrl + H'
+        }
+      ]
+    }
+  ]
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu);
+
+  const ctxMenu = new Menu()
+  ctxMenu.append(new MenuItem({
+    label: 'Hello',
+    click: function () {
+      console.log("welcome to context menu");
+    }
+  }))
+  ctxMenu.append(new MenuItem({ role: 'selectall' }))
+  mainWindow1.webContents.on('context-menu', function (e, params) {
+    ctxMenu.popup(mainWindow1, params.x, params.y)
+  })
+  globalShortcut.register('Alt+1', function () {
+    mainWindow1.show();
+  })
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
--- one.js
+++ one.js
@@ -1,9 +1,31 @@
 console.log('from one.js');
-
+const electron = require('electron')
+const ipc = electron.ipcRenderer
 const BrowserWindow = require("electron").remote.BrowserWindow;
 const newWindow = document.getElementById("newWindow");
-newWindow.addEventListener('click', function(event) {
+const errBtn = document.getElementById("errorBtn");
+const syncBtn = document.getElementById("syncBtn");
+newWindow.addEventListener('click', function (event) {
     let mainWindow3 = new BrowserWindow();
     mainWindow3.loadURL(`file://${__dirname}/three.html`);
     mainWindow3.webContents.openDevTools();
 })
+
+errBtn.addEventListener('click', function (event) {
+    console.log("123");
+    ipc.send('open-error-dialog')
+    console.log("456");
+
+})
+
+syncBtn.addEventListener('click', function (event) {
+    console.log("123");
+    const reply = ipc.sendSync('sync-message');
+    console.log(reply);
+    console.log("456");
+})
+
+ipc.on('opened-error-dialog', function (event, args) {
+    console.log(args);
+})
+
